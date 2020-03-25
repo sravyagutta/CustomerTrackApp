44ed4ce0eee3b9054d97cdb7f145faf940da0970
sravyagutta <49697056+sravyagutta@users.noreply.github.com>
2020-03-25 15:39:02 -0700

    Initial commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/CustomerController.java
@@ -0,0 +1,98 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Customer;
+import com.luv2code.springboot.thymeleafdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+	private CustomerService customerService;
+	
+	public CustomerController(CustomerService theCustomerService) {
+		customerService = theCustomerService;
+	}
+	
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		// get employees from db
+		List<Customer> theCustomers = customerService.findAll();
+		
+		// add to the spring model
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "customers/list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customers/customer-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+									Model theModel) {
+		
+		// get the employee from the service
+		Customer theCustomer = customerService.findById(theId);
+		
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("customer", theCustomer);
+		
+		// send over to our form
+		return "customers/customer-form";			
+	}
+	
+	
+	@PostMapping("/save")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		// save the employee
+		customerService.save(theCustomer);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/customers/list";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("customerId") int theId){
+		customerService.deleteById(theId);
+		return "redirect:/customers/list";
+		
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,60 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.thymeleafdemo.dao.CustomerRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	public CustomerServiceImpl(CustomerRepository theCustomerRepository) {
+		customerRepository = theCustomerRepository;
+	}
+	
+	@Override
+	public List<Customer> findAll() {
+		return customerRepository.findAllByOrderByLastNameAsc();
+	}
+
+	@Override
+	public Customer findById(int theId) {
+		Optional<Customer> result = customerRepository.findById(theId);
+		
+		Customer theCustomer = null;
+		
+		if (result.isPresent()) {
+			theCustomer = result.get();
+		}
+		else {
+			// we didn't find the employee
+			throw new RuntimeException("Did not find customer id - " + theId);
+		}
+		
+		return theCustomer;
+	}
+
+	@Override
+	public void save(Customer theCustomer) {
+		customerRepository.save(theCustomer);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		customerRepository.deleteById(theId);
+	}
+
+}
+
+
+
+
+
+
